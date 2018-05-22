a33144dbba402e434a17de6bd3dfd606eff7e65e
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2018-05-22 13:35:31 +0200

    Initial project setup

---
+++ app/src/main/java/nl/jovmit/infoapp/Guides.kt
@@ -0,0 +1,12 @@
+package nl.jovmit.infoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Guides : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guides)
+    }
+}
